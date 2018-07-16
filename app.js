daa6244cd24374955b65a7edae39fd0c76c17725
sebastian-kopiczko <sebastian.kopiczko@gmail.com>
2018-07-16 17:37:13 +0200

    displaying channel details and latest videos

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const channelForm = document.querySelector('#channel-form');
 const channelInput = document.querySelector('#channel-input');
 const channelData = document.querySelector('#channel-data');
 const videoContainer = document.querySelector('#video-container');
-const defaultChannel = 'techguyweb';
+
+// form subbmit and change chanel variable
+channelForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const channel = channelInput.value;
+    getChannel(channel);
+})
 
 //load auth2 lib
 function handleClientLoad() {
@@ -38,7 +44,6 @@ function updateSigninStatus(isSignedIn) {
         signoutButton.style.display = 'block';
         content.style.display = 'block';
         videoContainer.style.display = 'block';
-        getChannel(defaultChannel);
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
@@ -55,6 +60,14 @@ function handleLoginClick() {
 function handleLogoutClick() {
     gapi.auth2.getAuthInstance().signOut();
 }
+// display channel data
+function showChannelData(data) {
+    channelData.innerHTML = data;
+}
+//convert string to number with spaces
+function toLocalString(input){
+    return (Number(input)).toLocaleString();
+}
 // get channel from api
 function getChannel(channel) {
     gapi.client.youtube.channels.list({
@@ -63,6 +76,51 @@ function getChannel(channel) {
     })
     .then(response => {
         console.log(response);
+        const channel = response.result.items[0];
+        const output = `
+            <ul class="collection">
+                <li class="collection-item">Title: ${channel.snippet.title}</li>
+                <li class="collection-item">ID: ${channel.id}</li>
+                <li class="collection-item">Subscribers: ${toLocalString(channel.statistics.subscriberCount)}</li>
+                <li class="collection-item">Views: ${toLocalString(channel.statistics.viewCount)}</li>
+                <li class="collection-item">Videos: ${toLocalString(channel.statistics.videoCount)}</li>
+            </ul>
+            <p>${channel.snippet.description}</p>
+            <hr>
+            <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${channel.snippet.customUrl}">Visit Channel</a>
+        `;
+        showChannelData(output);
+        const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+        requestVideoPlaylist(playlistId);
     })
     .catch(err => alert('There is not channel by given name'));
+}
+
+function requestVideoPlaylist(playlistId) {
+    const requestOptions = {
+        playlistId: playlistId,
+        part: 'snippet',
+        maxResults: 10
+    }
+    const request = gapi.client.youtube.playlistItems.list(requestOptions);
+    request.execute(response => {
+        console.log(response);
+        const playlistItems = response.result.items;
+        if(playlistItems){
+            let output = '<h4 class="align-center">Latest videos</h4>'
+            // loop through videos and append output
+            playlistItems.forEach(item => {
+                const videoId = item.snippet.resourceId.videoId;
+                output += `
+                <div class="col s3">
+                    <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </div>
+                `;
+            });
+            // output videos
+            videoContainer.innerHTML = output;
+        } else{
+          videoContainer.innerHTML = 'No uploaded videos'  
+        }
+    });
 }
